66de7d21483185085ac34ac14b5f17466ad7bbc0
Shamsiddin Avezov <quyosh.ogli@gmail.com>
2019-07-04 17:34:46 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,76 +1,29 @@
 // PLEASE DON'T change function name
+//lets give constant objects to the value of coins that we are going to change
+const COIN = [
+  {type: 'H', value: 50},
+  {type: 'Q', value: 25},
+  {type: 'D', value: 10},
+  {type: 'N', value: 5},
+  {type: 'P', value: 1},
+];
+
 module.exports = function makeExchange(currency) {
-
-   var item = {error: "You are rich, my friend! We don't have so much coins for exchange"}
-    var items = [{"P":1},{"N":5},{"D":10},{"Q":25},{"H":50}];
-var capacity = currency;
-
-
-if (capacity<=10000){
-var idxItem   = 0,
-      idxWeight = 0,
-      oldMax    = 0,
-      newMax    = 0,
-      numItems  = items.length,
-      weightMatrix  = new Array(numItems+1),
-      keepMatrix    = new Array(numItems+1),
-      solutionSet   = [];
-
-  // Setup matrices
-  for(idxItem = 0; idxItem < numItems + 1; idxItem++){
-    weightMatrix[idxItem] = new Array(capacity+1);
-    keepMatrix[idxItem]   = new Array(capacity+1);
+  if (currency > 10000) {
+    return {error: 'You are rich, my friend! We don\'t have so much coins for exchange'}
   }
+  const result = COIN.reduce(change, {rest: currency});
+  delete result.rest;
+  return result;//returns changed value using ocjects
+};
 
-  // Build weightMatrix from [0][0] -> [numItems-1][capacity-1]
-  for (idxItem = 0; idxItem <= numItems; idxItem++){
-    for (idxWeight = 0; idxWeight <= capacity; idxWeight++){
+function change(changed, coin) {//function that change currency to the coins
+  const value = Math.floor(changed.rest / coin.value);
 
-      // Fill top row and left column with zeros
-      if (idxItem === 0 || idxWeight === 0){
-        weightMatrix[idxItem][idxWeight] = 0;
-      }
-
-      // If item will fit, decide if there's greater value in keeping it,
-      // or leaving it
-      else if (items[idxItem-1].w <= idxWeight){
-        newMax = items[idxItem-1].b + weightMatrix[idxItem-1][idxWeight-items[idxItem-1].w];
-        oldMax = weightMatrix[idxItem-1][idxWeight];
-
-        // Update the matrices
-        if(newMax > oldMax){ 
-          weightMatrix[idxItem][idxWeight]  = newMax;
-          keepMatrix[idxItem][idxWeight]    = 1;
-        }
-        else{
-          weightMatrix[idxItem][idxWeight]  = oldMax; 
-          keepMatrix[idxItem][idxWeight]    = 0;
-        }
-      }
-
-      // Else, item can't fit; value and weight are the same as before
-      else{
-        weightMatrix[idxItem][idxWeight] = weightMatrix[idxItem-1][idxWeight];
-      }
-    }
+  if (value > 0) {
+    changed[coin.type] = value;
   }
+  changed.rest = changed.rest % coin.value;
 
-  // Traverse through keepMatrix ([numItems][capacity] -> [1][?])
-  // to create solutionSet
-  idxWeight = capacity;
-  idxItem   = numItems;
-  for(idxItem; idxItem > 0; idxItem--){
-    if(keepMatrix[idxItem][idxWeight] === 1){
-      solutionSet.push(items[idxItem - 1]);
-      idxWeight = idxWeight - items[idxItem - 1].w;
-    }
-  }
-  return {"maxValue": weightMatrix[numItems][capacity], "set": solutionSet};
-
-
-
-}else {
-	return item;
-}
-   
+  return changed;//returns only changed data
 }
